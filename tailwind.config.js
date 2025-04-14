d7163cdc265cc848af229eaafad9ee17bca6588f
Mahika Trivedi <137401624+MahikaTrivedi@users.noreply.github.com>
2025-04-15 00:41:30 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",  // For the main HTML file
+    "./src/**/*.{js,ts,jsx,tsx}"  // For all JS/TS/JSX/TSX files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+  
